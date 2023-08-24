c857decebacf2b9e17f9a69325cf4fb3ccb78b31
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-08-24 08:31:04 +0100

    creating relationship

--- models.py
+++ models.py
@@ -3,18 +3,19 @@ from main import db
 
 class User(db.Model):
     __tablename__ = "users"
-    id = db.Column(db.Integer(),primary_key=True)
+    id = db.Column(db.Integer,primary_key=True)
     username =  db.Column(db.String(length=200), unique=True)
-    email = db.Column(db.String())
+    email = db.Column(db.String)
+    books = db.relationship('Book',backref='user',lazy=True)
 
     def __repr__(self):
         return f"UID:{self.id} {self.username}"
 
 class Book(db.Model):
     __tablename__  = "books"
-    id = db.Column(db.Integer(),primary_key=True)
-    locccn = db.Column(db.Integer())
-    title = db.Column(db.Integer(),unique=True)
-    # owner = db.relationship("User",backref = db.backref('books',lazy=True))
+    id = db.Column(db.Integer,primary_key=True)
+    locccn = db.Column(db.Integer)
+    title = db.Column(db.Integer,unique=True)
+    owner_id = db.Column(db.Integer,db.ForeignKey("user.id"),nullable=False)
     def __repr__(self):
         return self.title
