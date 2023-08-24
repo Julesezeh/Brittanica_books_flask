04397604ddae51492c16f290f932388846e04304
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-08-24 07:46:13 +0100

    separate model

---
+++ models.py
@@ -0,0 +1,20 @@
+from main import db
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer(),primary_key=True)
+    username =  db.Column(db.String(length=200), unique=True)
+    email = db.Column(db.String())
+
+    def __repr__(self):
+        return f"UID:{self.id} {self.username}"
+
+class Book(db.Model):
+    __tablename__  = "books"
+    id = db.Column(db.Integer(),primary_key=True)
+    locccn = db.Column(db.Integer())
+    title = db.Column(db.Integer(),unique=True)
+    # owner = db.relationship("User",backref = db.backref('books',lazy=True))
+    def __repr__(self):
+        return self.title
