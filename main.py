065366aa5023535f5e5ee117bec9d835344b925e
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-08-26 12:03:34 +0100

    validating args with reqparse

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 import os,json
-from flask_restx import Api,Resource
+from flask_restx import Api,Resource,reqparse,inputs
 
 
 base_dir = os.path.abspath(os.path.dirname(__file__))
@@ -21,46 +21,47 @@ db = SQLAlchemy(app)
 from models import User,Book
 
 todos = {}
+
+#Data validation
+user_parser = reqparse.RequestParser()
+user_parser.add_argument('username',type=str,required=True,help="Name can not be blank")
+user_parser.add_argument('email',type=inputs.email,required=False,help="Email is optional")
+
 #Views
-# @app.route("/",methods=["GET"])
-# def index():
-#     try:
-#         users = User.query.all()
-#         user_list = []
-#         for user in users:
-#             user_data = {
-#                 "id":user.id,
-#                 "username":user.username,
-#                 "email":user.email,
-#                 "books":user.books
-#             }
-#             user_list.append(user_data)
-#         # users = json.dumps(users)
-#         return jsonify({"users":user_list})
+
+@api.route("/api/users")
+class User(Resource):
+    def post(self):
+        args = user_parser.parse_args(request.json)
+        username = args['username']
+        email = args['email']
+        user = User(username=username,email=email)
+        db.session.add(user)
+        db.session.commit()
+        db.session.refresh(user)
+        return {"success":{"username":username,"email":email}}
+
+# @api.route("/todo")
+# class Todo_all(Resource):
+#     def get(self):
+#         users=  User.query.all()
+#         return users
+
+
+# @api.route("/todo/<int:todo_id>")
+# class Todo(Resource):
+#     def get(self,todo_id):
+#         return {"todo":todos[todo_id]},200
     
-#     except Exception as e:
-#         return {"Error":str(e)}
-
-@api.route("/todo")
-class Todo_all(Resource):
-    def get(self):
-        return todos
+#     def put(self,todo_id):
+#         todos[todo_id] = request.form['data']
+#         return {"todo":todos[todo_id]},200
 
 
-@api.route("/todo/<int:todo_id>")
-class Todo(Resource):
-    def get(self,todo_id):
-        return {"todo":todos[todo_id]}
-    
-    def put(self,todo_id):
-        todos[todo_id] = request.form['data']
-        return {"todo":todos[todo_id]}
-
-
-@api.route("/home")
-class HelloWorld(Resource):
-    def get(self):
-        return {"Hello":"World"}
+# @api.route("/home")
+# class HelloWorld(Resource):
+#     def get(self):
+#         return {"Hello":"World"},200
 
 if __name__ == "__main___":
     app.run(debug=True)
