14f4b67c114b5d182a18c7854ded8af9bb1d3523
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-08-27 12:17:03 +0100

    :.

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from .extensions import db,api
+
+def create_app():
+    
+    app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.sqlite3"
+
+    api.init_app(app)
+    db.init_app(app)
+
+    return app
