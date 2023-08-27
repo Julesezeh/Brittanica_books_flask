b3e6f6b6607b0efe027722b17da2b9bac2fe892f
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-08-27 13:18:46 +0100

    Book and User models created

---
+++ app/models.py
@@ -0,0 +1,15 @@
+from .extensions import db
+
+class User(db.model):
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(50), unique=True)
+
+    books = db.relationship("Book",back_populates="user")
+
+class Book(db.model):
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(70),unique=True)
+    user_id = db.Column(db.ForeignKey("user.id"))
+
+    user = db.relationship("User",back_populates="books")
+    
