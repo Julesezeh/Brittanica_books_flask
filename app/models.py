666a2530388f49e2e2f6816d09949ddc36ce07a9
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-08-27 15:07:47 +0100

    debugged model

--- app/models.py
+++ app/models.py
@@ -1,15 +1,14 @@
 from .extensions import db
 
-class User(db.model):
+class User(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(50), unique=True)
 
     books = db.relationship("Book",back_populates="user")
 
-class Book(db.model):
+class Book(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     title = db.Column(db.String(70),unique=True)
     user_id = db.Column(db.ForeignKey("user.id"))
 
     user = db.relationship("User",back_populates="books")
-    
