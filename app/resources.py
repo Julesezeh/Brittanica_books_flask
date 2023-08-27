9a62a4ba53d2125990f9b93e97146c2f90e2f2e1
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-08-27 15:08:12 +0100

    New endpoint and Resource handler for books

--- app/resources.py
+++ app/resources.py
@@ -2,9 +2,17 @@ from flask_restx import Resource,Namespace
 
 api_namespace = Namespace("api")
 
+from .models import Book
 
 @api_namespace.route("/hello")
 class Hello(Resource):
     #Simply define http methods
     def get(self):
         return {"Hello":"This is being served from rest-x"}
+    
+
+
+@api_namespace.route("/books")
+class BooksAPI(Resource):
+    def get(self):
+        return Book.query.all()
