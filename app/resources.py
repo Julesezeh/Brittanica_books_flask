717e3e756a528e133af79f492db80a8c5664422a
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-08-28 08:26:05 +0100

    book serializer

--- app/resources.py
+++ app/resources.py
@@ -1,8 +1,9 @@
 from flask_restx import Resource,Namespace
 
-api_namespace = Namespace("api")
-
+from .schema import book_schema
 from .models import Book
+
+api_namespace = Namespace("api")
 
 @api_namespace.route("/hello")
 class Hello(Resource):
@@ -14,5 +15,6 @@ class Hello(Resource):
 
 @api_namespace.route("/books")
 class BooksAPI(Resource):
+    @api_namespace.marshal_list_with(book_schema)
     def get(self):
         return Book.query.all()
