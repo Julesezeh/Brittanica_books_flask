136c1da7bd88dd9f54c4818935456c4ac80b6cbb
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-08-28 11:37:15 +0100

    schema (api model) for creating new users

--- app/schema.py
+++ app/schema.py
@@ -2,14 +2,20 @@ from flask_restx import fields
 
 from .extensions import api
 
+
 book_schema = api.model("Book",{
     'id':fields.Integer,
-    'title':fields.String
+    'title':fields.String,
+    # 'user':fields.Nested(user_schema)
 })
 
 user_schema = api.model("User",{
     "id":fields.Integer,
     "username":fields.String,
-    "books":fields.Nested(book_schema)
+    "books":fields.List(fields.Nested(book_schema))
 
+})
+
+user_creation_schema = api.model("UserCreation",{
+    "username":fields.String
 })
