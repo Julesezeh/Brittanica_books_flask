b64593c6cc4c1261f3fc250a7d5475e0afbcb378
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-08-28 13:15:58 +0100

    post method for user created

--- app/resources.py
+++ app/resources.py
@@ -1,7 +1,9 @@
 from flask_restx import Resource,Namespace
 
-from .schema import book_schema, user_schema
+from .schema import book_schema, user_schema, user_creation_schema
 from .models import Book, User
+
+from .extensions import db
 
 api_namespace = Namespace("api")
 
@@ -19,8 +21,18 @@ class BooksAPI(Resource):
     def get(self):
         return Book.query.all()
 
+
 @api_namespace.route("/users")    
 class UserAPI(Resource):
     @api_namespace.marshal_list_with(user_schema)
     def get(self):
         return User.query.all()
+    
+    @api_namespace.expect(user_creation_schema)
+    @api_namespace.marshal_with(user_schema)
+    def post(self):
+        print(api_namespace.payload)
+        new_user = User(username=api_namespace.payload["username"])
+        db.session.add(new_user)
+        db.session.commit()
+        return new_user,201
