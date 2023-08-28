72c4d821a65b0955c372d6f4c969f9ed8107e016
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-08-28 09:56:19 +0100

    schema for the user "api model"

--- app/schema.py
+++ app/schema.py
@@ -6,3 +6,8 @@ book_schema = api.model("Book",{
     'id':fields.Integer,
     'title':fields.String
 })
+
+user_schema = api.model("User",{
+    "id":fields.Integer,
+    "username":fields.String
+})
