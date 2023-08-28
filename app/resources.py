a68189ffdb0eb6bdfd06980b9fda125a112848f8
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-08-28 09:56:47 +0100

    Users get method endpoint configured.

--- app/resources.py
+++ app/resources.py
@@ -1,7 +1,7 @@
 from flask_restx import Resource,Namespace
 
-from .schema import book_schema
-from .models import Book
+from .schema import book_schema, user_schema
+from .models import Book, User
 
 api_namespace = Namespace("api")
 
@@ -18,3 +18,9 @@ class BooksAPI(Resource):
     @api_namespace.marshal_list_with(book_schema)
     def get(self):
         return Book.query.all()
+
+@api_namespace.route("/users")    
+class UserAPI(Resource):
+    @api_namespace.marshal_list_with(user_schema)
+    def get(self):
+        return User.query.all()
